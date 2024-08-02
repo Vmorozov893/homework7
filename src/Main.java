0b9f0c05c70241d6fccecf1169da42c59e251d04
vmorozov893 <vmorozov893@gmail.com>
2024-08-02 05:54:58 +0300

    2 Задания

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("1 Задание\n");
+
+        int[] arr1 = new int[3];
+        arr1[0] = 1;
+        arr1[1] = 2;
+        arr1[2] = 3;
+
+        float[] arr2 = {1.57F, 7.654F, 9.986F};
+
+        char[] arr3 = {'H', 'e', 'l', 'l', 'o'};
+
+        System.out.println("\n2 Задание\n");
+
+        for (int i = 0; i < 3; i++){
+            if (i != 2){
+                System.out.print(arr1[i] + ", ");
+            }
+            else{
+                System.out.print(arr1[i] + ".\n");
+            }
+        }
+
+        for (int i = 0; i < 3; i++){
+            if (i != 2){
+                System.out.print(arr2[i] + ", ");
+            }
+            else{
+                System.out.print(arr2[i] + ".\n");
+            }
+        }
+
+        for (int i = 0; i < arr3.length; i++){
+            if (i != arr3.length - 1){
+                System.out.print(arr3[i] + ", ");
+            }
+            else{
+                System.out.print(arr3[i] + ".\n");
+            }
+        }
 
 
 
