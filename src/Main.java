0d7ca93313db83e8b442ca8235ca42d3800fdff4
vmorozov893 <vmorozov893@gmail.com>
2024-08-02 06:09:41 +0300

    Готовое решение.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -41,7 +43,45 @@ public class Main {
             }
         }
 
+        System.out.println("\n3 Задание\n");
 
+        for (int i = 2; i >= 0; i--){
+            if (i != 0){
+                System.out.print(arr1[i] + ", ");
+            }
+            else{
+                System.out.print(arr1[i] + ".\n");
+            }
+        }
+
+        for (int i = 2; i >= 0; i--){
+            if (i != 0){
+                System.out.print(arr2[i] + ", ");
+            }
+            else{
+                System.out.print(arr2[i] + ".\n");
+            }
+        }
+
+        for (int i = arr3.length - 1; i >= 0; i--){
+            if (i != 0){
+                System.out.print(arr3[i] + ", ");
+            }
+            else{
+                System.out.print(arr3[i] + ".\n");
+            }
+        }
+
+        System.out.println("\n4 Задание\n");
+
+        for (int i = 0; i < arr1.length; i++){
+            if(arr1[i] % 2 == 1){
+                arr1[i] ++;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr1));
 
     }
+
 }
